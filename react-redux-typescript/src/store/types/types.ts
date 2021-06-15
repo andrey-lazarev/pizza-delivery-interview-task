24e82907787f26f071753a832b2203e96298c6ed
Andrey Lazarev <andrey.lazarev34@gmail.com>
2021-06-16 03:31:29 +0400

    update store/types/types.ts

--- react-redux-typescript/src/store/types/types.ts
+++ react-redux-typescript/src/store/types/types.ts
@@ -2,15 +2,22 @@ import { ThunkDispatch } from 'redux-thunk';
 import { Action } from "redux";
 
 export type TGetState = () => IAppState;
+
 export type Dispatch = ThunkDispatch<unknown, unknown, Action>
+
 export type ThunkAction = (
   dispatch: Dispatch,
   getState: TGetState
 ) => void;
 
+export type TMenuMockUp = {
+  name: string,
+  description: string
+}[]
+
 export enum ECurrency {
-    'dollar',
-    'euro',
+    dollar = 'dollar',
+    euro = 'euro',
 }
 
 export interface IUser {
