707e2c8dd112025ed5f3d1a643735575b5fa5328
Andrey Lazarev <andrey.lazarev34@gmail.com>
2021-06-16 03:33:37 +0400

    update store/helpers/getMenuMockUp.ts

--- react-redux-typescript/src/store/helpers/getMenuMockUp.ts
+++ react-redux-typescript/src/store/helpers/getMenuMockUp.ts
@@ -1,16 +1,16 @@
 import { v4 as uuid } from 'uuid';
-import { IMenu } from '../types/types';
+import { IMenu, TMenuMockUp } from '../types/types';
 import { getImageUrlByName } from './getImageUrlByName';
 import { genIntInRange } from './getIntInRange';
 
 export const getMenuMockUp = (
-  menuData: { name: string, description: string }[],
-): IMenu => {
-  return menuData.map((item) => ({
+  menuData: TMenuMockUp,
+): IMenu => ({
+  ...menuData.map((item) => ({
     [uuid()]: {
       ...item,
       price: genIntInRange(10, 50),
       imageUrl: getImageUrlByName(item.name)
     }
-  }));
-};
+  }))
+});
