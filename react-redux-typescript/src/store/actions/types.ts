3fca324c120e284204ff4ecb862fb96cb40ee0ea
Andrey Lazarev <andrey.lazarev34@gmail.com>
2021-06-16 11:14:07 +0400

    chore

--- react-redux-typescript/src/store/actions/types.ts
+++ react-redux-typescript/src/store/actions/types.ts
@@ -1,47 +1,47 @@
 import {
   ADD_ITEM_TO_CART,
   REMOVE_ITEM_FROM_CART,
-  SET_MODAL_IS_OPEN,
+  TOGGLE_MODAL_IS_OPEN,
   SET_ORDER,
   SET_USER,
   TOGGLE_CURRENCY,
 } from './constants';
 import { Order, User, UUID } from '../types/types';
 
-export interface IAddToCartAction {
+export interface AddItemToCart {
   type: typeof ADD_ITEM_TO_CART;
   id: UUID;
 }
 
-export interface IRemoveFromCartAction {
+export interface RemoveItemFromCart {
   type: typeof REMOVE_ITEM_FROM_CART;
   id: UUID;
 }
 
-export interface IToggleCurrencyAction {
+export interface ToggleCurrency {
   type: typeof TOGGLE_CURRENCY;
 }
 
-export interface ISetModalIsOpenAction {
-  type: typeof SET_MODAL_IS_OPEN;
+export interface ToggleModalIsOpen {
+  type: typeof TOGGLE_MODAL_IS_OPEN;
   isOpen: boolean;
 }
 
-export interface ISetOrderAction {
+export interface SetOrder {
   type: typeof SET_ORDER;
   order: Order;
 }
 
-export interface ISetUserAction {
+export interface SetUser {
   type: typeof SET_USER;
   user: User;
 }
 
-export type TAppAction = (
-  IAddToCartAction |
-  IRemoveFromCartAction |
-  IToggleCurrencyAction |
-  ISetModalIsOpenAction |
-  ISetOrderAction |
-  ISetUserAction
+export type AppAction = (
+  AddItemToCart |
+  RemoveItemFromCart |
+  ToggleCurrency |
+  ToggleModalIsOpen |
+  SetOrder |
+  SetUser
   )
